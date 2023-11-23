ddc6c03df1ca378c77a4a0ea1529d9bebd780016
Johnatan Augusto <augustosales2010@gmail.com>
2023-11-23 17:25:53 -0300

    Criação do bd e class friend

--- src/RedeSocialProjeto/DatabaseConnector.java
+++ src/RedeSocialProjeto/DatabaseConnector.java
@@ -1,6 +1,7 @@
 package RedeSocialProjeto;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
@@ -10,5 +11,52 @@ public class DatabaseConnector {
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
+
+    public static void InicializeDB(){
+        try (Connection connection = DatabaseConnector.connect()) {
+            String commandMessages = "DROP TABLE IF EXISTS messages CASCADE ";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(commandMessages)) {
+                preparedStatement.executeQuery();
+            }
+            
+            String commandFriends = "DROP TABLE IF EXISTS friends CASCADE ";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(commandFriends)) {
+                preparedStatement.executeQuery();
+            }
+            
+            String commandUsers = "DROP TABLE IF EXISTS users CASCADE ";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(commandUsers)) {
+                preparedStatement.executeQuery();
+            }
+            String createUsers = "CREATE TABLE users (\n" +
+                    "    id SERIAL PRIMARY KEY,\n" +
+                    "    name VARCHAR(255) NOT NULL,\n" +
+                    "    email VARCHAR(255) NOT NULL UNIQUE,\n" +
+                    "    password VARCHAR(255) NOT NULL,\n" +
+                    "    phone_number VARCHAR(20)\n" +
+                    ");";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(createUsers)) {
+                preparedStatement.executeQuery();
+            }
+            String createFriends = "CREATE TABLE friends (\n" +
+                    "    id SERIAL PRIMARY KEY,\n" +
+                    "    user_id int NOT NULL,\n" +
+                    "    id_amigo int NOT NULL\n" +
+                    ");";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(createFriends)) {
+                preparedStatement.executeQuery();
+            }
+            String createMessages = "CREATE TABLE messages (\n" +
+                    "    id SERIAL PRIMARY KEY,\n" +
+                    "    friend_id int NOT NULL,\n" +
+                    "    message VARCHAR(255)\n" +
+                    ");";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(createMessages)) {
+                preparedStatement.executeQuery();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/RedeSocialProjeto/MainClass.java
+++ src/RedeSocialProjeto/MainClass.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 public class MainClass {
     public static void main(String[] args) {
+        DatabaseConnector.InicializeDB();
         SwingUtilities.invokeLater(() -> {
             new LoginScreen();
         });
